25fdb175cfa32cb3b053aa0ae513b284fc5a5573
zakifkr <147596681+zakifkr@users.noreply.github.com>
2024-04-30 14:57:44 +0700

    Update graf.py

--- graf.py
+++ graf.py
@@ -1,4 +1,29 @@
-#bagian Zaki
+graf = {
+    'Brebes' : ['Tegal', 'Slawi'],
+    'Tegal' : ['Brebes', 'Slawi', 'Pemalang'],
+    'Slawi' : ['Brebes', 'Tegal', 'Purwokerto'],
+    'Purwokerto' : ['Slawi', 'Cilacap', 'Kroya', 'Kebumen', 'Purbalingga'],
+    'Pemalang' : ['Tegal', 'Purbalingga', 'Pekalongan'],
+    'Purbalingga' : ['Purwokerto', 'Pemalang', 'Banjarnegara'],
+    'Pekalongan' : ['Pemalang', 'Kendal'],
+    'Cilacap' : ['Purwokerto', 'Kroya'],
+    'Kroya' : ['Cilacap', 'Purwokerto', 'Kebumen'],
+    'Kebumen' : ['Purwokerto', 'Kroya', 'Purworejo']
+}
+
+def perjalanan(graf, awal, akhir, jalur=[]):
+    jalur = jalur + [awal]
+    if awal == akhir:
+        return [jalur]
+    if not awal in graf:
+        return []
+    seluruh_jalur = []
+    for x in graf[awal]:
+        if x not in jalur:
+            rute = perjalanan(graf, x, akhir, jalur)
+            for rute_baru in rute:
+                seluruh_jalur.append(rute_baru)
+    return seluruh_jalur
 
 #bagian Yosi
 
