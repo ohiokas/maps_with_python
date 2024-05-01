4c3c3d6c8d9a6cbc129871714b5da13d1264d774
YosionBestyMarpaung <147596895+YosionBestyMarpaung@users.noreply.github.com>
2024-05-01 14:29:11 +0700

    Update graf.py

--- graf.py
+++ graf.py
@@ -25,9 +25,31 @@ def perjalanan(graf, awal, akhir, jalur=[]):
                 seluruh_jalur.append(rute_baru)
     return seluruh_jalur
 
-#bagian Yosi
 
-#bagian Vikas
+def jumlah_rute(jumlah):
+    rute = ''
+    for x in range(0, len (jumlah)):
+        if x < len(jumlah) -1:
+            rute += jumlah[x] + '->'
+        else:
+            rute += jumlah[x]
+    return rute
+#MAIN PROGRAM
+tempat_awal = str(input('Masukkan Kota Asal: '))
+tempat_akhir = str(input('Masukkan Kota Tujuan: '))
+
+pencarian = perjalanan(graf, tempat_awal, tempat_akhir)
+print('Jumlah Rute: ', len(pencarian))
+print()
+min = pencarian[0]
+max = []
+for x in pencarian:
+    if len(x) < len(min):
+        min = x
+    elif len(x) > len(max):
+        max = x
+
+
 r = [
     'Rute Tercepat : ', jumlah_rute(min),
     '',
